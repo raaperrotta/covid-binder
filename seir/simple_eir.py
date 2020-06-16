44a66417e34b06b4ee0d29e0715f6dc81bab0242
Robert Perrotta <raaperrotta@gmail.com>
2020-06-16 11:41:01 -0400

    Added mostly working nevergrad/pymc SDE with HTML

--- seir/simple_eir.py
+++ seir/simple_eir.py
@@ -13,12 +13,16 @@ data = pd.read_csv(
     parse_dates=['data'],
 )
 
+# The data for some regions might be causing problems. There are few cases here and the
+# number recovered lags the deaths in the early days that suggests reporting dynamics not captured by this model.
+# That could also be overcome by increasing the low value uncertainty but with as few cases as they have it would
+# probably wash out any effect these regions have on the model.
 ds = (
     data
     .set_index(['denominazione_regione', 'data'])
     .drop(columns=['lat', 'long', 'codice_regione', 'stato', 'note_it', 'note_en', 'casi_testati'])
     .to_xarray()
-)
+).drop_sel(denominazione_regione=['Basilicata', 'Marche', 'Molise', "Valle d'Aosta"])
 region_names = list(ds['denominazione_regione'].values)
 
 t = ds['data'].values
@@ -26,8 +30,8 @@ n_time = len(t)
 n_regions = len(ds['denominazione_regione'])
 
 era_starts = np.array(pd.to_datetime([
-    '1 Mar 2020',
-    #     '8 Mar 2020',
+    # '1 Mar 2020',
+    '8 Mar 2020',
     '21 Mar 2020',
     '14 Apr 2020',
     '4 May 2020',
@@ -51,12 +55,14 @@ max_threshold = 1e7
 
 
 def ode(initial_exposed, initial_infectious, beta, beta_detected_ratio, sigma, theta,
-        gamma_mu, gamma_detected, mu_detected):
+        gamma_mu, gamma_detected, mu_detected, mu_ratio):
     beta = beta[..., era_indices]
     beta_detected_ratio = beta_detected_ratio[..., era_indices]
     # Limit possible % detections to avoid negative infectious cases
     # If % detected in a single day is ever actually this high we should make a better model for theta
     theta = np.minimum(0.95, theta * 1e-2 * sqrt_new_tests)
+
+    mu_ratio = np.concatenate([[1], mu_ratio])[era_indices]
 
     exposed = np.inf * np.ones((n_regions, n_time))
     infectious = np.inf * np.ones((n_regions, n_time))
@@ -78,7 +84,8 @@ def ode(initial_exposed, initial_infectious, beta, beta_detected_ratio, sigma, t
         progressed = sigma * exposed[..., i - 1]
         detections = theta[..., i - 1] * infectious[..., i - 1]
         recoveries_detected = gamma_detected * detected[..., i - 1]
-        deaths_detected = mu_detected * detected[..., i - 1]
+        # deaths_detected = mu_detected * detected[..., i - 1]
+        deaths_detected = mu_detected * mu_ratio[i - 1] * detected[..., i - 1]
 
         exposed_d = newly_exposed - progressed
         infectious_d = progressed - detections - gamma_mu * infectious[..., i - 1]
@@ -125,13 +132,13 @@ def calc_score(exposed, infectious, detected, recovered, dead):
     score += x.sum()
 
     y = ds['deceduti'].values
-    var = 10 ** 2 + y * y * (0.05 ** 2)
+    var = 5 ** 2 + y * y * (0.1 ** 2)
     x = y - dead
     x = x * x / var
     score += x.sum()
 
     y = ds['dimessi_guariti'].values
-    var = 10 ** 2 + y * y * (0.1 ** 2)
+    var = 40 ** 2 + y * y * (0.2 ** 2)
     x = y - recovered
     x = x * x / var
     score += x.sum()
@@ -139,21 +146,22 @@ def calc_score(exposed, infectious, detected, recovered, dead):
     return score
 
 
-def transform(initial_exposed, initial_infectious, beta, beta_detected_ratio, sigma, theta, gamma_mu, gamma_detected, mu_detected):
+def transform(initial_exposed, initial_infectious, beta, beta_detected_ratio, sigma, theta, gamma_mu, gamma_detected, mu_detected, mu_ratio):
     initial_exposed = np.exp(initial_exposed)
     initial_infectious = np.exp(initial_infectious)
     beta = np.exp(beta)
     beta_detected_ratio = (np.tanh(beta_detected_ratio) + 1) / 2
     theta = np.exp(theta)
     mu_detected = np.exp(mu_detected)
-    return initial_exposed, initial_infectious, beta, beta_detected_ratio, sigma, theta, gamma_mu, gamma_detected, mu_detected
+    mu_ratio = (np.tanh(mu_ratio) + 3) / 4  # in (0.5, 1)
+    return initial_exposed, initial_infectious, beta, beta_detected_ratio, sigma, theta, gamma_mu, gamma_detected, mu_detected, mu_ratio
 
 
-def func(initial_exposed, initial_infectious, beta, beta_detected_ratio, sigma, theta, gamma_mu, gamma_detected, mu_detected):
+def func(initial_exposed, initial_infectious, beta, beta_detected_ratio, sigma, theta, gamma_mu, gamma_detected, mu_detected, mu_ratio):
     return calc_score(
         *ode(
             *transform(
-                initial_exposed, initial_infectious, beta, beta_detected_ratio, sigma, theta, gamma_mu, gamma_detected, mu_detected
+                initial_exposed, initial_infectious, beta, beta_detected_ratio, sigma, theta, gamma_mu, gamma_detected, mu_detected, mu_ratio
             )
         )
     )
@@ -161,20 +169,21 @@ def func(initial_exposed, initial_infectious, beta, beta_detected_ratio, sigma, 
 
 def main():
 
-    beta = 0.2 * np.ones((n_regions, n_eras))
+    beta = 0.05 * np.ones((n_regions, n_eras))
     beta_detected_ratio = 0.5 * np.ones((n_regions, n_eras))
-    sigma = 0.1
-    theta = 0.01 * np.ones((n_regions, 1))
+    sigma = 0.2
+    theta = 0.1 * np.ones((n_regions, 1))
     gamma_mu = 0.1
-    gamma_detected = 0.2
+    gamma_detected = 0.1
     mu_detected = 0.1 * np.ones(n_regions)
+    mu_ratio = np.ones(n_eras-1)
 
     initial_exposed = 200 * np.ones(n_regions)
     initial_infectious = 20 * np.ones(n_regions)
 
     print('Initial guess:', calc_score(*ode(initial_exposed, initial_infectious, beta,
                                             beta_detected_ratio, sigma, theta, gamma_mu,
-                                            gamma_detected, mu_detected)))
+                                            gamma_detected, mu_detected, mu_ratio)))
 
     instrum = ng.p.Instrumentation(
         initial_exposed=ng.p.Array(init=np.log(initial_exposed)),
@@ -185,52 +194,78 @@ def main():
         theta=ng.p.Array(init=np.log(theta)),
         gamma_mu=ng.p.Log(init=gamma_mu),
         gamma_detected=ng.p.Log(init=gamma_detected),
-        mu_detected=ng.p.Array(init=np.log(mu_detected)),
+        mu_detected=ng.p.Array(init=mu_detected),
+        mu_ratio=ng.p.Array(init=mu_ratio * 5),
     )
     print('Initial instrumentation:', func(**instrum.kwargs))
 
-    # # Or use last solution as starting point
-    # with open('italy_eir4.pkl', 'rb') as f:
-    #     kwargs = pickle.load(f)
-    # beta = kwargs['beta']
-    # beta_detected_ratio = kwargs['beta_detected_ratio']
-    # sigma = kwargs['sigma']
-    # theta = kwargs['theta']
-    # gamma_mu = kwargs['gamma_mu']
-    # gamma_detected = kwargs['gamma_detected']
-    # mu_detected = kwargs['mu_detected']
-    # initial_exposed = kwargs['initial_exposed']
-    # initial_infectious = kwargs['initial_infectious']
+    # Or use last solution as starting point
+    with open('tmp_best21.pkl', 'rb') as f:
+        kwargs = pickle.load(f)
+    beta = kwargs['beta']
+    beta_detected_ratio = kwargs['beta_detected_ratio']
+    sigma = kwargs['sigma']
+    theta = kwargs['theta']
+    gamma_mu = kwargs['gamma_mu']
+    gamma_detected = kwargs['gamma_detected']
+    mu_detected = kwargs['mu_detected']
+    mu_ratio = kwargs['mu_ratio']
+    initial_exposed = kwargs['initial_exposed']
+    initial_infectious = kwargs['initial_infectious']
+
+    print('Last solution:', func(initial_exposed, initial_infectious, beta, beta_detected_ratio,
+                                 sigma, theta, gamma_mu, gamma_detected, mu_detected, mu_ratio))
+
+    # sigma /= 1.2
+    # gamma_mu /= 1.2
+
+    # beta = np.delete(beta, [-1], axis=1)
+    # beta_detected_ratio = np.delete(beta_detected_ratio, [-1], axis=1)
+    # mu_ratio = np.delete(mu_ratio, [-1], axis=0)
     #
-    # print('Last solution:', func(initial_exposed, initial_infectious, beta, beta_detected_ratio,
-    #                             sigma, theta, gamma_mu, gamma_detected, mu_detected))
-    #
-    # instrum = ng.p.Instrumentation(
-    #     initial_exposed=ng.p.Array(init=initial_exposed),
-    #     initial_infectious=ng.p.Array(init=initial_infectious),
-    #     beta=ng.p.Array(init=beta),
-    #     beta_detected_ratio=ng.p.Array(init=beta_detected_ratio),
-    #     sigma=ng.p.Log(init=sigma),
-    #     theta=ng.p.Array(init=theta),
-    #     gamma_mu=ng.p.Log(init=gamma_mu),
-    #     gamma_detected=ng.p.Log(init=gamma_detected),
-    #     mu_detected=ng.p.Array(init=mu_detected),
-    # )
-    # print('Initial instrumentation:', func(**instrum.kwargs))
+    # # Replace bad regions stuck in weird space
+    # for replace_me, replace_with in [
+    #     # ('P.A. Trento', 'Liguria'),
+    #     # ('P.A. Bolzano', 'Liguria'),
+    #     # ('Abruzzo', 'Friuli Venezia Giulia'),
+    #     # ('Campania', 'Calabria'),
+    #     # ('Sicilia', 'Veneto'),
+    #     # ('Lombardia', 'Piemonte'),
+    #     # ('Piemonte', 'Puglia'),
+    #     # ('Umbria', 'Veneto'),
+    # ]:
+    #     replace_me = region_names.index(replace_me)
+    #     replace_with = region_names.index(replace_with)
+    #     beta[replace_me] = beta[replace_with]
+    #     beta_detected_ratio[replace_me] = beta_detected_ratio[replace_with]
+    #     theta[replace_me] = theta[replace_with]
+    #     mu_detected[replace_me] = mu_detected[replace_with]
+
+    # print('Perturbed solution:', func(initial_exposed, initial_infectious, beta, beta_detected_ratio,
+    #                                   sigma, theta, gamma_mu, gamma_detected, mu_detected, mu_ratio))
+
+    instrum = ng.p.Instrumentation(
+        initial_exposed=ng.p.Array(init=initial_exposed),
+        initial_infectious=ng.p.Array(init=initial_infectious),
+        beta=ng.p.Array(init=beta),
+        beta_detected_ratio=ng.p.Array(init=beta_detected_ratio),
+        sigma=ng.p.Log(init=sigma),
+        theta=ng.p.Array(init=theta),
+        gamma_mu=ng.p.Log(init=gamma_mu),
+        gamma_detected=ng.p.Log(init=gamma_detected),
+        mu_detected=ng.p.Array(init=mu_detected),
+        mu_ratio=ng.p.Array(init=mu_ratio),
+    )
+    print('Initial instrumentation:', func(**instrum.kwargs))
 
     # recommendation = ng.optimizers.TwoPointsDE(instrum, budget=1_000).minimize(func)
     # print('Early recommendation:', func(**recommendation.kwargs))
 
     kwargs, best_kwargs = util.fit_nevergrad_model(
-        instrum, 2_000_000, ng.optimizers.TwoPointsDE, func, num_workers=48, num_processes=6, save_after=1_000
+        instrum, 1_000_000, ng.optimizers.MultiCMA, func, num_workers=9, num_processes=9, save_after=1_000
     )
     print('Recommendation:', func(**kwargs))
     print('Best scorer:', func(**best_kwargs))
-
-    # initial_exposed, initial_infectious, beta, beta_detected_ratio, sigma, theta, gamma_mu, gamma_detected, mu_detected = transform(**recommendation.kwargs)
-    # exposed, infectious, detected, recovered, dead = ode(initial_exposed, initial_infectious, beta,
-    #                                                      beta_detected_ratio, sigma,
-    #                                                      theta, gamma_mu, gamma_detected, mu_detected)
 
 
 if __name__ == '__main__':
